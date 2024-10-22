8113ce865cbec55b1413549954cb16501b0bdd33
Muhammad Shofa <mshofa09@gmail.com>
2024-10-22 18:04:17 +0700

    Add database files and create README.md file

---
+++ README.md
@@ -0,0 +1,21 @@
+-- Pengguna/role
+1 admin
+3 client
+
+-- Fitur Client
+
+- Dapat login menggunakan 1 dari 3 client yang sudah disediakan
+- Dapat memilih kandidat yang tersedia
+- Muncul konfirmasi dan jeda waktu sebelum menginsert ke database
+- Setelah menginsert halaman pemilihan siap digunakan kembali tanpa reload
+- Hak akses ke halaman pemilihan saja, tidak dapat masuk ke halaman admin
+- On/Off Fullscreen
+- Logout
+
+-- Fitur Admin
+
+- Dapat login menggunakan akun yang sudah tersedia
+- Hak akses ke halaman admin dan pemilihan
+- Dapat melihat jumlah dan persentase suara secara realtime
+- On/Off Fullscreen
+- Logout
---
+++ pemilihan_ketua_osis.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 22, 2024 at 10:56 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pemilihan_ketua_osis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `computer_name` varchar(50) DEFAULT NULL,
+  `role` enum('admin','client') NOT NULL DEFAULT 'client'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `computer_name`, `role`) VALUES
+(1, 'komputer1', 'komputer10123', 'Komputer 1', 'client'),
+(2, 'komputer2', 'komputer20123', 'Komputer 2', 'client'),
+(3, 'komputer3', 'komputer30123', 'Komputer 3', 'client'),
+(4, 'admin', 'admin0123', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `vote_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `kandidat_id` int NOT NULL,
+  `waktu_vote` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`vote_id`, `user_id`, `kandidat_id`, `waktu_vote`) VALUES
+(107, 1, 2, '2024-10-22 10:51:43'),
+(108, 1, 1, '2024-10-22 10:51:59'),
+(109, 1, 1, '2024-10-22 10:52:27'),
+(110, 1, 1, '2024-10-22 10:53:07'),
+(111, 1, 1, '2024-10-22 10:53:32'),
+(112, 1, 1, '2024-10-22 10:54:20'),
+(113, 1, 2, '2024-10-22 10:55:03');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`vote_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `vote_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=114;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
